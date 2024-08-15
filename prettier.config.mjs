aa272c0e3019c43e207603f7065688657cfbf6d9
ricardollu <29088213+ricardollu@users.noreply.github.com>
2024-08-15 13:26:14 +0800

    init

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+}
+
+export default config
