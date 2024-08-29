50235403df3ea6f87a0fe02a1b5967bc2a26fb51
ricardollu <29088213+ricardollu@users.noreply.github.com>
2024-08-29 16:32:18 +0800

    fix: bgm.tv data not load on firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['storage'],
+    permissions: ['storage', 'https://api.bgm.tv/*'],
   },
   vite: () => ({
     resolve: {
